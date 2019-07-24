2846a760efa689985adfd6ef4a4a76836a92cfd5
Nathan Pierce <nathan.pierce@block.one>
2019-07-24 13:59:55 -0400

    fixes

--- .cicd/package-builder.sh
+++ .cicd/package-builder.sh
@@ -16,13 +16,13 @@ else
         echo 'Fedora family detected, building for RPM.'
         [[ -z $ARTIFACT ]] && ARTIFACT='*.rpm'
         PACKAGE_TYPE='rpm'
-        execute mkdir -p ~/rpmbuild/BUILD
-        execute mkdir -p ~/rpmbuild/BUILDROOT
-        execute mkdir -p ~/rpmbuild/RPMS
-        execute mkdir -p ~/rpmbuild/SOURCES
-        execute mkdir -p ~/rpmbuild/SPECS
-        execute mkdir -p ~/rpmbuild/SRPMS
-        execute sudo yum install -y rpm-build
+        mkdir -p ~/rpmbuild/BUILD
+        mkdir -p ~/rpmbuild/BUILDROOT
+        mkdir -p ~/rpmbuild/RPMS
+        mkdir -p ~/rpmbuild/SOURCES
+        mkdir -p ~/rpmbuild/SPECS
+        mkdir -p ~/rpmbuild/SRPMS
+        sudo yum install -y rpm-build
     elif [[ $ID == 'amzn' ]]; then
         echo "SKIPPED: We do not generate $NAME packages since they use rpms created from Centos."
         exit 0
@@ -38,8 +38,8 @@ BASE_COMMIT=$(cat build/programs/nodeos/config.hpp | grep 'version' | awk '{prin
 BASE_COMMIT="${BASE_COMMIT:2:42}"
 echo "Found build against $BASE_COMMIT."
 cd build/packages
-execute chmod 755 ./*.sh
-execute ./generate_package.sh "$PACKAGE_TYPE"
+chmod 755 ./*.sh
+./generate_package.sh "$PACKAGE_TYPE"
 [[ -d x86_64 ]] && cd 'x86_64' # backwards-compatibility with release/1.6.x
 execute buildkite-agent artifact upload "./$ARTIFACT"
 for A in $(echo $ARTIFACT | tr ';' ' '); do
