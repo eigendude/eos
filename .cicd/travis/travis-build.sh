5e9af7a9c4bc635d5a6afc2976caa6eb063683a0
Nathan Pierce <nathan.pierce@block.one>
2019-07-26 10:46:14 -0400

    fixes

--- .cicd/travis/travis-build.sh
+++ .cicd/travis/travis-build.sh
@@ -17,5 +17,5 @@ if [[ "$(uname)" == Darwin ]]; then
     if $ENABLE_SUBMODULE_REGRESSION_TEST; then cd $ROOT_DIR && fold-execute ./.cicd/submodule-regression-checker.sh; fi
 else # linux
     DOCKER_RUN_EXTRAS="-e ENABLE_PACKAGE_BUILDER=false" # Travis doesn't need to test or push packages
-    execute eval docker run --rm -v $(pwd)/..:/workdir -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e CCACHE_DIR=/opt/.ccache -e TRAVIS $DOCKER_RUN_EXTRAS $FULL_TAG
+    execute eval docker run --rm -v $(pwd):/workdir -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e CCACHE_DIR=/opt/.ccache -e TRAVIS $DOCKER_RUN_EXTRAS $FULL_TAG
 fi
