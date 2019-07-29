c5f88c172158b447fc62e1921b5283bcd018670b
Nathan Pierce <nathan.pierce@block.one>
2019-07-29 17:17:33 -0400

    fixes

--- .cicd/travis/travis-build.sh
+++ .cicd/travis/travis-build.sh
@@ -9,12 +9,11 @@ pwd
 CPU_CORES=$(getconf _NPROCESSORS_ONLN)
 if [[ "$(uname)" == Darwin ]]; then
     ./.cicd/docker/.helpers-v33
-    mkdir -p build && cd build && cmake ..
-    make -j$(getconf _NPROCESSORS_ONLN)
+    mkdir -p build && cd build && fold-execute cmake ..
+    fold-execute make -j$(getconf _NPROCESSORS_ONLN)
     if ${ENABLE_PARALLEL_TESTS:-true}; then fold-execute ctest -j$(getconf _NPROCESSORS_ONLN) -LE _tests --output-on-failure -T Test; fi
     if ${ENABLE_SERIAL_TESTS:-true}; then mkdir -p ./mongodb && fold-execute mongod --dbpath ./mongodb --fork --logpath mongod.log && fold-execute ctest -L nonparallelizable_tests --output-on-failure -T Test; fi
     if ${ENABLE_LR_TESTS:-false}; then fold-execute ctest -L long_running_tests --output-on-failure -T Test; fi
-    if ${ENABLE_PACKAGE_BUILDER:-false}; then cd $ROOT_DIR && fold-execute ./.cicd/package-builder.sh; fi
     if ${ENABLE_SUBMODULE_REGRESSION_TEST:-true}; then cd $ROOT_DIR && fold-execute ./.cicd/submodule-regression-checker.sh; fi
 else # linux
     DOCKER_RUN_EXTRAS="-e ENABLE_PACKAGE_BUILDER=false" # Travis doesn't need to test or push packages
